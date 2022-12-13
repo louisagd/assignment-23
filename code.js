9990fa53fe3f4e9a0305ca24ac4467e2c9390b54
louisagd <116597362+louisagd@users.noreply.github.com>
2022-12-13 15:14:58 -0600

    Add files via upload

---
+++ code.js
@@ -0,0 +1,11 @@
+function hello(name) {
+    alert("hello " + name);
+}
+hello("Can you find the secret message?");
+
+function secretMessage(message){
+console.log("The secret message is"+ message);
+
+}
+
+secretMessage("Goat Cheese");
